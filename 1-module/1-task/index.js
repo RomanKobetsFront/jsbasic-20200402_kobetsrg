130c7b75a5d408a501c753ce45e9608011444b92
Kobets Roman <rgkobets@gmail.com>
2020-04-10 18:11:23 +0300

    module 0, module 1 tasks 1,2,3

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,14 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  if (n == 1 || n == 0) {
+    return 1;
+  } else {
+    let count = n - 1;
+    while (count) {
+      n *= count;
+      count--;
+    }
+    return n;
+  }
 }
--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -4,21 +4,26 @@
 function print(text) {
   console.log(text);
 }
+// *имя не пустое, без пробелов, минимум 4 символа*.
 
 /**
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (!name || name.includes(" ") || name.length < 4) {
+    return false;
+  }
+
+  return true;
 }
 
 function sayHello() {
-  const userName = prompt('Введите ваше имя');
+  const userName = prompt("Введите ваше имя");
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+    print("Некорректное имя");
   }
 }
